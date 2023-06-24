23837fa0a60685c8433d7621ddade03c61e3c980
happyme531 <2302004040@qq.com>
2023-06-24 21:57:44 +0800

    Stop the current generation job on new  request
    Better responsiveness now.
    Require https://github.com/oobabooga/text-generation-webui/pull/2848

--- index.js
+++ index.js
@@ -2,7 +2,19 @@ const express = require('express')
 const app = express()
 const axios = require('axios')
 
-const NEW_API_URI = 'http://127.0.0.1:5000/api/v1/generate'
+
+const NEW_API_BASE = 'http://127.0.0.1:5000/api/v1'
+
+let pendingRequestCount = 0;
+let lastRequestId = 0;
+
+function delay(ms){
+  return new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+      resolve();
+    }, ms)
+  })
+}
 
 app.use(express.json())
 
@@ -15,7 +27,7 @@ app.post('/generate', async (req, res) => {
     temperature: oldData.parameters.temperature,
     top_p: oldData.parameters.top_p,
     typical_p: 1,
-    repetition_penalty: 1.18, 
+    repetition_penalty: 1.18,
     top_k: 4,
     min_length: 0,
     no_repeat_ngram_size: 0,
@@ -28,20 +40,40 @@ app.post('/generate', async (req, res) => {
     truncation_length: 2048,
     ban_eos_token: false,
     skip_special_tokens: false,
-    stopping_strings: [] 
+    stopping_strings: []
   }
 
   try {
-    console.log("Running new API")
-    const response = await axios.post(NEW_API_URI, newData) 
-    const result = response.data.results[0].text 
-    console.log("Result: ", result) 
-    res.json({ generated_text: result })
+    const requestId = ++lastRequestId;
+    const requestIdStr = "<" + String(requestId) + "> ";
+    if (pendingRequestCount > 0) {
+      let response = await axios.post(NEW_API_BASE + "/stop-stream", {});
+      if (response.data.results === "success") {
+        console.log(requestIdStr + "Last request stopped successfully.");
+      } else {
+        console.warn(requestIdStr + `Failed to stop stream ${JSON.stringify(response)}`)
+      }
+      await delay(150); 
+      response = await axios.post(NEW_API_BASE + "/stop-stream", {}); // Workaround for the imperfect implemention in server
+      if (response.data.results === "success") {
+        console.log(requestIdStr + "Last request stopped successfully_2.");
+      } else {
+        console.warn(requestIdStr + `Failed to stop stream ${JSON.stringify(response)}`)
+      }
+    }
+    pendingRequestCount++;
+    console.log(requestIdStr + "Running new API")
+    const response = await axios.post(NEW_API_BASE + "/generate", newData)
+    const result = response.data.results[0].text
+    console.log(requestIdStr + "Result: ", result);
+    res.json({ generated_text: result });
+    pendingRequestCount--;  
+
 
   } catch (err) {
-    console.error("Error: ", err.message)
+    console.error(requestIdStr + "Error: ", err.message)
     res.status(500).send({ error: err.message })
   }
-}) 
+})
 
 app.listen(3000)
